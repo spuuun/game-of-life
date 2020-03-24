e0a2287e840f20a29f528fb7aca7c016ae44b9b0
Matthew Ross <matthew.douglas.ross@gmail.com>
2020-03-24 17:15:36 -0400

    delete old screen.cs file

--- screen.cs
+++
@@ -1,4 +0,0 @@
-// inherits grid properties from world class
-
-// an array of cells (from nextState method in world class)
-    // and passes them to a "print" method - essentially displaying the grid
