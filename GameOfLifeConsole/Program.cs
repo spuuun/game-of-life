0c06ba88126d711d3ad51d72456a30263e1e79f8
Matthew Ross <matthew.douglas.ross@gmail.com>
2020-03-29 13:52:10 -0400

    changed refs to RenderGrid, which now belongs to screen class

--- GameOfLifeConsole/Program.cs
+++ GameOfLifeConsole/Program.cs
@@ -5,107 +5,25 @@ namespace GameOfLifeConsole
 {
     class Program
     {
-        // static int tableWidth = 73;
-
         static void Main(string[] args)
         {
+            Console.Write("Grid Side Length? > ");
+            var sideLength = Int32.Parse(Console.ReadLine());
+            Screen seed = new Screen(sideLength, sideLength);
+
+            Console.Write("Number of Iteration? > ");
+            var iterations = Int32.Parse(Console.ReadLine());
+
             World world = new World();
-            Screen screen = new Screen(10, 10);
-            world.RenderGrid(screen);
-            world.Tick(screen);
-            world.Tick(screen);
-            world.Tick(screen);
-            world.Tick(screen);
-            world.Tick(screen);
 
+            seed.RenderGrid();
 
+            Screen newSeed = world.Iterate(seed);
 
-
-
-
-            // Console.Clear();
-            // PrintLine();
-            // PrintRow("Column 1", "Column 2", "Column 3", "Column 4");
-            // PrintLine();
-            // PrintRow("", "", "", "");
-            // PrintRow("", "", "", "");
-            // PrintLine();
-            // Console.ReadLine();
+            for (var i = 0; i < iterations - 1; i++)
+            {
+                newSeed = world.Iterate(newSeed);
+            }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-// public static void RenderGrid(Screen screen)
-// {
-// render grid of living and dead cells
-// var screen0 = new List<List<Cell>>();
-// var Width = 35;
-// var Height = 35;
-
-// for (var y = 0; y < Height; y++)
-// {
-//     var sublist = new List<Cell>();
-//     for (var x = 0; x < Width; x++)
-//     {
-//         var newCell = new Cell(x, y)
-//         {
-//             Y = y,
-//             X = x
-//         };
-//         sublist.Add(newCell);
-//     }
-//     screen0.Add(sublist);
-// }
-// }
-
-
-
-
-
-
-
-
-
-//         static void PrintLine()
-//         {
-//             Console.WriteLine(new string('-', tableWidth));
-//         }
-
-//         static void PrintRow(params string[] columns)
-//         {
-//             int width = (tableWidth - columns.Length) / columns.Length;
-//             string row = "|";
-
-//             foreach (string column in columns)
-//             {
-//                 row += AlignCentre(column, width) + "|";
-//             }
-
-//             Console.WriteLine(row);
-//         }
-
-//         static string AlignCentre(string text, int width)
-//         {
-//             text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
-
-//             if (string.IsNullOrEmpty(text))
-//             {
-//                 return new string(' ', width);
-//             }
-//             else
-//             {
-//                 return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
-//             }
-//         }
-//     }
-// }
-
