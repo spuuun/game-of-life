4270dfd84022355f463d19c7412d588d0559b693
Matthew Ross <matthew.douglas.ross@gmail.com>
2020-04-09 19:05:10 -0400

    modify cell

--- GameOfLifeConsole/Cell.cs
+++ GameOfLifeConsole/Cell.cs
@@ -10,7 +10,7 @@ namespace GameOfLifeConsole
         public bool isAlive { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
-        public int LivingNeighbors { get; set; }
+        //public int LivingNeighbors { get; set; }
         public String PrintedValue
         {
             get
@@ -30,28 +30,28 @@ namespace GameOfLifeConsole
         {
             X = x;
             Y = y;
-            LivingNeighbors = 0;
+            //LivingNeighbors = 0;
         }
 
-        public void Tick()
-        {
-            if ((this.LivingNeighbors < 2 || this.LivingNeighbors > 3) && this.isAlive)
-            {
-                this.isAlive = false;
-            }
-            else if (this.LivingNeighbors == 3 && !this.isAlive)
-            {
-                this.isAlive = true;
-            }
-            else
-            {
-                this.isAlive = this.isAlive;
-            }
-        }
-        public void ClearNeighbors()
-        {
-            // clear number of living neighbors before next gen.
-            LivingNeighbors = 0;
-        }
+        //public void Tick()
+        //{
+        //    if ((this.LivingNeighbors < 2 || this.LivingNeighbors > 3) && this.isAlive)
+        //    {
+        //        this.isAlive = false;
+        //    }
+        //    else if (this.LivingNeighbors == 3 && !this.isAlive)
+        //    {
+        //        this.isAlive = true;
+        //    }
+        //    else
+        //    {
+        //        this.isAlive = this.isAlive;
+        //    }
+        //}
+        //public void ClearNeighbors()
+        //{
+        //    // clear number of living neighbors before next gen.
+        //    LivingNeighbors = 0;
+        //}
     }
 }
